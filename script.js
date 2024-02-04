20ff6da9856be61328f934272492c41547856e59
Ayen <87607693+Ayen11@users.noreply.github.com>
2024-02-04 21:16:37 +0200

    added QR and button to open it

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+document.addEventListener("DOMContentLoaded", function () {
+  var overlay = document.getElementById("overlay");
+  var overlayImage = document.getElementById("overlay-image");
+  var closeButton = document.getElementById("close-button");
+  var openImageButton = document.getElementById("open-image-btn");
 
+  openImageButton.addEventListener("click", function () {
+    // Set the image source dynamically
+    overlayImage.src = "images/PortfolioQR.png";
+    // Display the overlay
+    overlay.style.display = "flex";
+  });
 
+  closeButton.addEventListener("click", function () {
+    // Hide the overlay when close button is clicked
+    overlay.style.display = "none";
+  });
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+  overlay.addEventListener("click", function (event) {
+    // Hide the overlay when clicking outside the image
+    if (event.target === overlay) {
+      overlay.style.display = "none";
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add('active-tab');
-}
-
-
-var sidemenu = document.getElementById("sidemenu");
-
-    function openmenu(){
-        sidemenu.style.right = "0px";
-    } 
-    
-    function closemenu(){
-        if (screen.width <= 600) {
-        sidemenu.style.right = "-200px";
-        };
-    } 
+  });
+});
